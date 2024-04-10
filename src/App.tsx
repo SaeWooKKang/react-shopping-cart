45db5a63a990d330822169a90ed68045829749c5
saewookkang <pjy9665@kakao.com>
2024-04-10 15:57:08 +0900

    feat: set tanstack query & devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
 function App() {
-  return <div>hello react!</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>hello react!</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
