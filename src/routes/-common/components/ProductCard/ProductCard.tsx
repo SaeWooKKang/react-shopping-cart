1dbcfb1b32588907549f512919d3a23894eade24
saewookkang <pjy9665@kakao.com>
2024-04-19 22:24:12 +0900

    feat(ProductCard.tsx): 장바구니 저장 기능 추가

--- src/routes/-common/components/ProductCard/ProductCard.tsx
+++ src/routes/-common/components/ProductCard/ProductCard.tsx
@@ -1,17 +1,25 @@
 import type { ProductList } from '@/routes/-product-list.api'
 
+import { useCartListStore } from '../../store/cartListStore'
+
 type ProductCardProps = ProductList[number]
 
 export const ProductCard = (props: ProductCardProps) => {
+  const cartListStore = useCartListStore()
+
   return (
     <>
       <img src={props.imageUrl} alt={props.name} />
+
       <div className="flex justify-between p-5">
         <div className="product-info">
           <span className="product-info__name">{props.name}</span>
           <span className="product-info__price">{props.price.toLocaleString()}</span>
         </div>
-        <img src={'/src/assets/svgs/cart.svg'} alt="장바구니" />
+
+        <button className="pointer" onClick={() => cartListStore.saveProduct(props.id)}>
+          <img src={'/src/assets/svgs/cart.svg'} alt="장바구니" />
+        </button>
       </div>
     </>
   )
