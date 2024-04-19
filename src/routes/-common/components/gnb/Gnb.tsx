9952ff96bb04353dc4e2a30181902ba512a44a79
saewookkang <pjy9665@kakao.com>
2024-04-19 22:24:48 +0900

    design & feat(Gnb.tsx): 장바구니 갯수 노출

--- src/routes/-common/components/gnb/Gnb.tsx
+++ src/routes/-common/components/gnb/Gnb.tsx
@@ -1,26 +1,55 @@
 import { Link } from '@tanstack/react-router'
 
+import { useCartListStore } from '../../store/cartListStore'
+
 /**
  * @summary 공통으로 사용하는 네비게이션 바
  * @detail 메인페이지, 장바구니, 주문목록 페이지 라우팅
  */
-export const Gnb = () => (
-  <div>
-    <nav className="nav flex justify-around">
-      <div className="flex-center">
-        <Link to="/" className="nav-title pointer">
-          Foo Shop
-        </Link>
-      </div>
+export const Gnb = () => {
+  const cartListStore = useCartListStore()
 
-      <div className="flex gap-15">
-        <Link to="/cart" className="nav-button flex items-center pointer">
-          장바구니
-        </Link>
-        <Link to="/orders" className="nav-button flex items-center pointer">
-          주문목록
-        </Link>
-      </div>
-    </nav>
-  </div>
-)
+  return (
+    <div>
+      <nav className="nav flex justify-around">
+        <div className="flex-center">
+          <Link to="/" className="nav-title pointer">
+            Foo Shop
+          </Link>
+        </div>
+
+        <div className="flex gap-15">
+          <div className="flex" style={{ position: 'relative' }}>
+            <Link to="/cart" className="nav-button flex items-center pointer">
+              장바구니
+            </Link>
+
+            <em
+              style={{
+                position: 'absolute',
+                right: -12,
+                top: 10,
+                color: '#2ac1bc',
+                fontWeight: 'bold',
+                fontSize: 17,
+                width: 25,
+                height: 25,
+                backgroundColor: '#fff',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: '50%',
+              }}
+            >
+              {cartListStore.cartList.length}
+            </em>
+          </div>
+
+          <Link to="/orders" className="nav-button flex items-center pointer">
+            주문목록
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
