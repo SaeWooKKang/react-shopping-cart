6e873141585b42d6a4da15631ee9ab27253f058d
saewookkang <pjy9665@kakao.com>
2024-04-20 21:04:50 +0900

    feat: 무한스크롤 작업을 위한 상품 리스트 api 모킹

--- src/mocks/products/products.handlers.ts
+++ src/mocks/products/products.handlers.ts
@@ -1,18 +1,33 @@
 import { delay, http, HttpResponse } from 'msw'
 
-import type { ProductList } from '@/routes/-product-list.api'
+import type { ProductListResponse } from '@/routes/-product-list.api'
 
+import { PAGE_META } from '../const'
 import products from './products.json'
 
-const allProducts = new Map<number, ProductList[number]>(
+const allProducts = new Map<number, ProductListResponse['list'][number]>(
   products.map((product) => [product.id, product])
 )
 
 export const productsHandlers = [
-  http.get('/products', async () => {
-    await delay(1000)
+  http.get('/products', async ({ request }) => {
+    await delay(500)
 
-    return HttpResponse.json(Array.from(allProducts.values()))
+    const url = new URL(request.url)
+
+    const page = parseInt(url.searchParams.get('page') ?? PAGE_META.DEFAULT_PAGE)
+    const perCount = parseInt(url.searchParams.get('perCount') ?? PAGE_META.DEFAULT_PER_COUNT)
+
+    const start = page * perCount
+    const end = start + perCount
+
+    const maxPage = Math.ceil(allProducts.size / perCount)
+    const nextPage = page < maxPage ? page + 1 : undefined
+
+    return HttpResponse.json({
+      list: Array.from(allProducts.values()).slice(start, end),
+      meta: { next_page: nextPage },
+    })
   }),
   http.get('/products/:id', async ({ params }) => {
     await delay(1000)
