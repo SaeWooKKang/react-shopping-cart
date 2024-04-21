36ae884d3ac0feb574314fbb6d971246e1379707
saewookkang <pjy9665@kakao.com>
2024-04-21 19:02:31 +0900

    fix(mocks/browser.ts): msw 모킹 경고 문구 제거

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,13 @@
+import { http } from 'msw'
 import { setupWorker } from 'msw/browser'
 
 import { handlers } from './handlers'
 
-export const worker = setupWorker(...handlers)
+const imageExtensionRegex = /\.(png|jpe?g|gif|svg)$/i
+const sourceCodeExtensionRegex = /\.(ts|tsx|js|jsx)$/i
+
+export const worker = setupWorker(
+  ...handlers,
+  http.get(imageExtensionRegex, (req) => {}),
+  http.get(sourceCodeExtensionRegex, (req) => {})
+)
